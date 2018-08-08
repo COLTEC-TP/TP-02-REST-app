0659208c7931237f7e12adcec23d5522b2e8414f
kingnowdo <36502982+kingnowdo@users.noreply.github.com>
2018-08-08 16:11:49 -0300

    Token anotado e Retrofit importado

--- Projeto/app/src/main/java/com/gui_rei/tempopreparar/MainActivity.java
+++ Projeto/app/src/main/java/com/gui_rei/tempopreparar/MainActivity.java
@@ -3,7 +3,10 @@ package com.gui_rei.tempopreparar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String token = "1300a7ef84802f53402e16f9505ae2be"; //Ex: apiadvisor.climatempo.com.br/api/v1/forecast/locale/3477/days/15?token=
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
