b7d4ce83d0a25fe04c1e646267fdeac47f88903a
kingnowdo <36502982+kingnowdo@users.noreply.github.com>
2018-08-08 16:46:22 -0300

    Exemplo de usos

--- Projeto/app/src/main/java/com/gui_rei/tempopreparar/MainActivity.java
+++ Projeto/app/src/main/java/com/gui_rei/tempopreparar/MainActivity.java
@@ -6,7 +6,13 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String token = "1300a7ef84802f53402e16f9505ae2be"; //Ex: apiadvisor.climatempo.com.br/api/v1/forecast/locale/3477/days/15?token=
+    private static final String token = "1300a7ef84802f53402e16f9505ae2be";
+        //Ex:
+        // atual: http://apiadvisor.climatempo.com.br/api/v1/weather/locale/6879/current?token=
+        // 15 dias (ou 6, n entendi): apiadvisor.climatempo.com.br/api/v1/forecast/locale/6879/days/15?token=
+
+    private static final String bh_id = "6879";
+    private static final String sp_id = "3477";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
