32064b885b6a09a7529e8f681e5f6e8c4812f5e8
kingnowdo <36502982+kingnowdo@users.noreply.github.com>
2018-08-12 16:51:20 -0300

    Pode alterar a cidade

--- Projeto/app/src/main/java/com/gui_rei/tempopreparar/CityActivity.java
+++ Projeto/app/src/main/java/com/gui_rei/tempopreparar/CityActivity.java
@@ -30,26 +30,27 @@ public class CityActivity extends Activity {
 
         RetrofitConfig retrofitConfig = new RetrofitConfig();
         final BuscaCidadeService serviceA = retrofitConfig.getBuscaCidadeService();
-        Call<BuscaCidade> request = serviceA.getCidadeByName(cidade);
+        Call<BuscaCidade[]> request = serviceA.getCidadeByName(cidade);
         Toast.makeText(CityActivity.this,"Buscando por essa cidade", Toast.LENGTH_SHORT).show();
-        request.enqueue(new Callback<BuscaCidade>() {
+        request.enqueue(new Callback<BuscaCidade[]>() {
             @Override
-            public void onResponse(Call<BuscaCidade> call, Response<BuscaCidade> response) {// executado quando resposta for recebida
-                BuscaCidade cidade = response.body();
-                if(cidade.getName()!=null){
-                    Prefs prefs = Prefs.getInstance();
-                    prefs.setCity(cidade.getId());
-                    Toast.makeText(CityActivity.this,"Cidade " + cidade.getName() + ", " + cidade.getState() + "selecionada",Toast.LENGTH_SHORT).show();
-                    finish();
+            public void onResponse(Call<BuscaCidade[]> call, Response<BuscaCidade[]> response) {// executado quando resposta for recebida
+                BuscaCidade[] resultados = response.body();
+                BuscaCidade cidade = resultados[0];
+                Prefs prefs = Prefs.getInstance();
+                prefs.setCity(cidade.getId());
+
+                if(resultados.length>1){ //"Em 20 anos dessa industria vital isso nunca me ocorreu"
+                    Toast.makeText(CityActivity.this,"Encontramos mais de uma cidade com esse nome e selecionamos a primeira",Toast.LENGTH_SHORT).show();
                 }
-                else {
-                    Toast.makeText(CityActivity.this,"Cidade não encontrada",Toast.LENGTH_SHORT).show();
-                }
+                else Toast.makeText(CityActivity.this,"Cidade " + cidade.getName() + ", " + cidade.getState() + " selecionada",Toast.LENGTH_SHORT).show();
+
+                finish(); //fechar essa tela e voltar para a anterior
             }
             @Override
-            public void onFailure(Call<BuscaCidade> call, Throwable t) {//executado quando houver erros
+            public void onFailure(Call<BuscaCidade[]> call, Throwable t) {//executado quando houver erros
                 t.printStackTrace();
-                Toast.makeText(CityActivity.this,"Algo deu errado",Toast.LENGTH_SHORT).show();
+                Toast.makeText(CityActivity.this,"Algo deu errado, esta cidade pode não existir",Toast.LENGTH_SHORT).show();
             }
         });
     }
--- Projeto/app/src/main/java/com/gui_rei/tempopreparar/rest/city/BuscaCidadeService.java
+++ Projeto/app/src/main/java/com/gui_rei/tempopreparar/rest/city/BuscaCidadeService.java
@@ -10,9 +10,9 @@ import com.gui_rei.tempopreparar.rest.RetrofitConfig;
 //api/v1/locale/city?name=juiz%20de%20fora&state=MG&token=1300a7ef84802f53402e16f9505ae2be
 public interface BuscaCidadeService {
     @GET("api/v1/locale/city?token=" + RetrofitConfig.TOKEN)
-    public Call<BuscaCidade> getCidadeByName(@Query("name") String cityName);
+    public Call<BuscaCidade[]> getCidadeByName(@Query("name") String cityName);
 
     @GET("api/v1/locale/city?token=" + RetrofitConfig.TOKEN)
-    public Call<BuscaCidade> getCidadeByState(@Query("state") String stateName);
+    public Call<BuscaCidade[]> getCidadeByState(@Query("state") String stateName);
 
 }
