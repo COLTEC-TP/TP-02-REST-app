a010217d50f9dbb165d8858324d3479b5a591dab
Guilherme Assis <guildeassis@gmail.com>
2018-08-21 19:02:39 -0300

    Detalhes de cada dia (nao estilizado)

--- Projeto/app/src/main/java/com/gui_rei/tempopreparar/ClimaDetalhesActivity.java
+++ Projeto/app/src/main/java/com/gui_rei/tempopreparar/ClimaDetalhesActivity.java
@@ -26,7 +26,39 @@ public class ClimaDetalhesActivity extends Activity {
 
         String cidade = dias.getName() + ", " + dias.getState();
 
-        TextView texto = findViewById(R.id.detalhesInfo);
-        texto.setText(cidade + "\n" + dados.getDate_br() + "\n" + "max: " + dados.getTemperature().getMax());
+        TextView texto = findViewById(R.id.cidade);
+        texto.setText(cidade);
+
+        //Setar a data
+        TextView data = findViewById(R.id.data);
+        data.setText("Data: " + dados.getDate_br());
+
+        //Setar a temperatura
+        TextView temp = findViewById(R.id.temp);
+        temp.setText("Temperatura: " + dados.getTemperature().getMin() + " / " + dados.getTemperature().getMax());
+
+        //Setar a umidade
+        TextView umidade = findViewById(R.id.umidade);
+        umidade.setText("Umidade: " + dados.getHumidity().getMin() + " / " + dados.getHumidity().getMax());
+
+        //Setar a sensacao termica
+        TextView sensacao = findViewById(R.id.sensacao);
+        sensacao.setText("Sensação Térmica: " + dados.getThermal_sensation().getMin() + " / " + dados.getThermal_sensation().getMax());
+
+        //Setar UV
+        TextView uv = findViewById(R.id.uv);
+        uv.setText("UV: " + dados.getUv().getMax());
+
+        //Setar chance de chuva
+        TextView probabilidadeChuva = findViewById(R.id.propabilidadeChuva);
+        probabilidadeChuva.setText("Probabilidade de chuva: " + dados.getRain().getProbability() + "%");
+
+        //Setar preciptacao de chuva
+        TextView precipitacaoChuva = findViewById(R.id.precipitacaoChuva);
+        precipitacaoChuva.setText("Precipitacao de chuva: " + dados.getRain().getPrecipitation());
+
+        //Setar velocidade do vento
+        TextView velocidadeVento = findViewById(R.id.vento);
+        velocidadeVento.setText("Velocidade do vento: " + dados.getWind().getVelocity_avg());
     }
 }
